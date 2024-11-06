eba540381f6627e4677b7ebd43cacb2f9224a713
hcollingwood <hannah.collingwood@telespazio.com>
2024-11-06 08:32:16 +0000

    EODHP-643 - update run consumer

--- eodhp_utils/runner.py
+++ eodhp_utils/runner.py
@@ -1,6 +1,6 @@
 import os
 
-from pulsar import Client
+from pulsar import Client, ConsumerDeadLetterPolicy, ConsumerType
 
 
 def run(messagers_dict: dict, subscription_name: str):
@@ -10,7 +10,19 @@ def run(messagers_dict: dict, subscription_name: str):
 
     topics = list(messagers_dict.keys())
 
-    consumer = client.subscribe(topic=topics, subscription_name=subscription_name)
+    max_redelivery_count = 3
+    delay_ms = 30000
+
+    consumer = client.subscribe(
+        topic=topics,
+        subscription_name=subscription_name,
+        consumer_type=ConsumerType.Shared,
+        dead_letter_policy=ConsumerDeadLetterPolicy(
+            max_redeliver_count=max_redelivery_count,
+            dead_letter_topic=f"dead-letter-annotations-ingester",  # noqa:F541
+        ),
+        negative_ack_redelivery_delay_ms=delay_ms,
+    )
 
     while True:
         pulsar_message = consumer.receive()
@@ -19,7 +31,7 @@ def run(messagers_dict: dict, subscription_name: str):
 
         messager = messagers_dict[topic_name]
 
-        failures = messager.consume(pulsar_message)
+        failures = messager.process_update(pulsar_message)
 
         if failures.any_temporary():
             pulsar_message.negative_acknowledge()
